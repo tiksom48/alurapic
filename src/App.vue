d2c3947383ba5e514172c9f5769363e8f5fac49d
Jonathan Patrich <jonathanpatrich@outlook.com>
2022-12-27 09:26:29 -0300

    aula:4

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,75 @@
+
 <template>
-  <div>
-<h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-<ul>
+<input type="search" class="filtro" placeholder="filtre por parte do titulo">
 
-<li v-for="foto of fotos" v-bind:key="foto.url">
-  <img :src="foto.url" :alt="foto.titulo">
-</li>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" v-bind:key="foto.url">
 
-</ul>
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+      </li>
+    </ul>
 
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
 
 export default {
 
+components: {
+  'meu-painel' : Painel
+},
+
   data() {
-
     return {
+      titulo: "AluraPic",
+      fotos: [],
+    };
+  },
 
-      titulo: 'AluraPic',
-      fotos: [
-      {
-        url: 'https://fotografiamais.com.br/wp-content/uploads/2018/09/fotos-de-cachorro-existe-mercado.jpg',
-        titulo: 'cachorro'
-      },
-      {
-        url: 'https://fotografiamais.com.br/wp-content/uploads/2018/09/fotos-de-cachorro-existe-mercado.jpg',
-        titulo: 'cachorrão'
-      }
-    ]
-    }
-  }
-}
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then((res) => res.json())
+      .then((fotos) => (this.fotos = fotos),
+            (err) => console.log(err)
+      );
+  },
+};
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
