07ce16da64b55b458df50f71f77fb35347b02f8e
Jonathan Patrich <jonathanpatrich@outlook.com>
2023-01-03 14:43:57 +0044

    feat:Curso2/Aula2

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -35,6 +35,7 @@
   
   import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
   import Botao from '../shared/botao/Botao.vue';
+  import Foto from '@/domain/foto/Foto';
   
   export default {
   
@@ -43,25 +44,18 @@
       'imagem-responsiva': ImagemResponsiva, 
       'meu-botao': Botao
     },
+
     data() {
         return {
-            foto: {
-                titulo: '',
-                url: '',
-                descricao: ''
+            foto: new Foto() 
             }
-        }
     },
+
     methods: {
         grava() {
-            console.log('enviar dados para API');
-
-            this.foto = {
-                titulo: '',
-                url: '',
-                descricao: '' 
-            }
-
+          this.$http
+            .post('http://localhost:3000/v1/fotos', this.foto)
+            .then(() => this.foto = new Foto(), err => console.log(err));
         }
     }
   }
